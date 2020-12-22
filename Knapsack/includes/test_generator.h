ffb992bb14431c7aa1c24cdf50e09291354be0c0
Jerma066 <ragimov.ib@phystech.edu>
2020-12-22 15:34:58 +0300

    Test generation errors fix

--- Knapsack/includes/test_generator.h
+++ Knapsack/includes/test_generator.h
@@ -6,8 +6,8 @@
 
 namespace Tests {
 
-std::vector<np::KItem> GenSimpleTest(size_t n, size_t capacity,
-                                     size_t res_profit, std::string fname);
+std::vector<np::KItem> GenSimpleTest(size_t n, size_t max_elem_weight,
+                                     size_t max_elem_profit, std::string fname);
 std::vector<np::KItem> GenRandomTest(size_t n, size_t capacity,
                                      std::string fname);
 
